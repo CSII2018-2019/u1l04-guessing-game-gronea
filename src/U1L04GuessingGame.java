6c9a4bc5cf0504e859953560b5456a7526ec4312
gronea <gronea@LR064W7M-1718.ellis.int>
2018-09-27 08:30:29 -0400

    Pseudo code

--- src/U1L04GuessingGame.java
+++ src/U1L04GuessingGame.java
@@ -1,9 +1,21 @@
+import javax.swing.JOptionPane;
 
 public class U1L04GuessingGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int  answer = 1;
+		int guess = 0;
+		
+		do {
+			//Ask for input
+			String input = JOptionPane.showInputDialog("Take a guess between 1 and 100: ");
+			double num = Double.parseDouble(input);
+			guess = (int) num;
+		} while (guess != answer || guess != -1);
+			//Check input for the right answer or the negative 1
+			//Add to number of guesses
+			//Print a score at the end (i.e. how many guess)
+			//Implement a l
 	}
 
 }
